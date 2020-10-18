f4b7ccbaad3423e8922995626005d61d9a6ec6da
abdullah fathan <58077766+fathankun@users.noreply.github.com>
2020-10-18 07:31:41 +0700

    fixxing the led code
    i hope it helped

--- smart_home_led/smart_home_led.ino
+++ smart_home_led/smart_home_led.ino
@@ -1,9 +1,20 @@
-void setup() {
-  // put your setup code here, to run once:
+#define BLYNK_PRINT Serial // digunakan untuk monitor blynk di serial monitor
 
+#include <ESP8266WiFi.h> // file untuk modul wifi
+#include <BlynkSimpleEsp8266.h> // file untuk blynk
+
+char auth[] = "***************************"; 
+char ssid[] = " Your Hotspot SSID "; 
+char pass[] = " You Wifi Hotspot "; 
+
+void setup() // fungsi utama, sekali dijalankan
+{
+  Serial.begin(9600); // untuk monitor di serial print
+  Blynk.begin(auth, ssid, pass); // menjalankan blynk sesuai token, wifi, dan paswordnya
+  
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop() // fungsi utama, berulang kali dijalankan
+{
+  Blynk.run(); // perintah untuk menjalankan blynk 
 }
